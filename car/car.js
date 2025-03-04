33741009aad26fbe95dc3dc26bc537a361408e9c
Georgy9925 <golubov.g2@mail.ru>
2025-03-04 22:21:42 +0300

    sintaxis

---
+++ car/car.js
@@ -0,0 +1,32 @@
+const carName = "Toyota";
+
+alert(carName);
+
+let maxCarSpeed = "120 км/ч";
+
+alert(maxCarSpeed);
+
+maxCarSpeed = "130 км/ч";
+console.log('У машины изменилась скорость', maxCarSpeed);
+
+maxCarSpeed = "140 км/ч";
+console.log('У машины изменилась скорость', maxCarSpeed);
+
+maxCarSpeed = "150 км/ч";
+console.log('У машины изменилась скорость', maxCarSpeed);
+
+let carOwner = "Георгий";
+
+alert(carOwner);
+
+carOwner = "Олег";
+console.log('У машины изменился владелец', carOwner);
+
+carOwner = "Андрей";
+console.log('У машины изменился владелец', carOwner);
+
+carOwner = "Генадий";
+console.log('У машины изменился владелец', carOwner);
+
+
+
---
+++ sintaxis/index.js
@@ -0,0 +1,19 @@
+//JavaScript-код с созданием переменных
+
+/*Автор:
+Георгий Голубов*/
+
+const myName = "Георгий";
+console.log(myName);
+
+const mySurname = "Голубов";
+console.log(mySurname);
+
+let myFavoriteDrink = "кола";
+console.log(myFavoriteDrink);
+
+let myFavoriteAnimal = "кошка";
+console.log(myFavoriteAnimal);
+
+let myFavoriteProgrammingLanguage = "JavaScript";
+console.log(myFavoriteProgrammingLanguage);
