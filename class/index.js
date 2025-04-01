48ea31d81476f3e9d8bf2efe8e9ee88a0fd31232
Georgy9925 <golubov.g2@mail.ru>
2025-04-02 00:42:43 +0300

    class

---
+++ class/index.js
@@ -0,0 +1,26 @@
+class Student {
+    constructor(
+        name, 
+        age, 
+    ) {
+    this.name = name;
+    this.age = age;
+    this.technologies = [];
+    this.status = 'Junior';
+    }
+
+    setTechnologies(technologies) {
+      this.technologies = [
+        ...this.technologies,
+        ...technologies,
+      ];
+	}
+    setNewStatus (newStatus) {
+      this.status = newStatus;
+    }
+}
+
+const student = new Student ('Maxim', 20);
+student.setTechnolewogies([ 'HTML', 'CSS', 'JavaScript' ]);
+student.setNewStatus('Middle');
+console.log(student);
